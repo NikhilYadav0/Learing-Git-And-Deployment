37b059cf20e87e97a083deee7786aab3b5458e2b
NikhilYadav0 <nikhily.725@gmail.com>
2019-03-19 20:48:20 +0530

    commit 1

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express=require('express')
 const server=express()
-
+const path=require('path')
 server.use(express.json())
 server.use(express.urlencoded({extended:true}))
+const server_port=process.env.PORT || 3333
 
-
-server.listen(2706,()=>{
-    console.log(' server start at http://localhost:2706')
+server.use('/',express.static(path.join(__dirname,'public')))
+server.listen(server_port,()=>{
+    console.log(server_port)
 })
